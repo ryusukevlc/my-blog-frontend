e8b0d81a4de73595eb8fa1c6ce7934e03a73da1e
ryusuke.arata <ryusuke.arata1130@gmail.com>
2023-02-19 15:20:42 +0900

    [add]ハンバーガーメニューの開閉処理を追加

--- src/app/components/shared/header/header.component.ts
+++ src/app/components/shared/header/header.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
+  // ハンバーガーメニューの開閉状態（true: 開いている, false: 閉じている）
+  public isOpened: boolean = false;
 
+  public toggleHamburgerMenu() {
+    this.isOpened = !this.isOpened;
+  }
 }
