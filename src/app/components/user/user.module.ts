f5f4b5a375a339a6dd9cb3933a59dd9b97fb2b3f
ryusuke.arata <ryusuke.arata1130@gmail.com>
2023-10-29 23:41:22 +0900

    feat: user/articles/article-listコンポーネントを追加

--- src/app/components/user/user.module.ts
+++ src/app/components/user/user.module.ts
@@ -5,9 +5,10 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ArticleDetailComponent } from './articles/article-detail/article-detail.component';
 import { HomeComponent } from './home/home.component';
 import { TagsComponent } from './tags/tags.component';
+import { ArticleListComponent } from './articles/article-list/article-list.component';
 
 @NgModule({
-  declarations: [ArticleDetailComponent, HomeComponent, TagsComponent],
+  declarations: [ArticleDetailComponent, HomeComponent, TagsComponent, ArticleListComponent],
   imports: [CommonModule, FormsModule, ReactiveFormsModule],
 })
 export class UserModule {}
