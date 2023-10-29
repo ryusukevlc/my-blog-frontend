a939c59f937514d46fd195b334131c13a0077eb6
ryusuke.arata <ryusuke.arata1130@gmail.com>
2023-10-29 23:30:31 +0900

    feat: ユーザー側のタグ画面用コンポーネントを追加

--- src/app/components/user/user.module.ts
+++ src/app/components/user/user.module.ts
@@ -4,9 +4,10 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { ArticleDetailComponent } from './article-detail/article-detail.component';
 import { HomeComponent } from './home/home.component';
+import { TagsComponent } from './tags/tags.component';
 
 @NgModule({
-  declarations: [ArticleDetailComponent, HomeComponent],
+  declarations: [ArticleDetailComponent, HomeComponent, TagsComponent],
   imports: [CommonModule, FormsModule, ReactiveFormsModule],
 })
 export class UserModule {}
